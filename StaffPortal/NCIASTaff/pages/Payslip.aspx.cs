ba72b53bcaba155153d3c387aa5538d37884d4a3
moraadiana <moraadiana13@gmail.com>
2025-01-14 11:45:43 +0300

    update

--- StaffPortal/NCIASTaff/pages/Payslip.aspx.cs
+++ StaffPortal/NCIASTaff/pages/Payslip.aspx.cs
@@ -97,8 +97,65 @@ namespace NCIASTaff.pages
                 ex.Data.Clear(); 
             }
         }
-
         private void ViewPayslip()
+        {
+            try
+            {
+                string username = Session["username"].ToString();
+                var filename = Session["username"].ToString().Replace(@"/", @"");
+                var month = ddlMonth.SelectedValue;
+
+                if (month == "12")
+                {
+                    month = "12";
+
+                }
+                else if (month == "11")
+                {
+                    month = "11";
+                }
+                else if (month == "10")
+                {
+                    month = "10";
+                }
+                else if (month == "")
+                {
+                    month = "01";
+                }
+                else
+                {
+                    month = "0" + month;
+                }
+
+                var myDate = month + "/01/" + ddlYear.SelectedValue;
+                var period = DateTime.ParseExact(myDate, "M/dd/yyyy", CultureInfo.InvariantCulture);
+
+                var filePath = Server.MapPath("~/Downloads/") + String.Format("PAYSLIP-{0}.pdf", filename);
+
+                // Check if directory exists, if not create it
+                if (!Directory.Exists(Server.MapPath("~/Downloads/")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Downloads/"));
+                }
+                webportals.GeneratePayslipReport4(username, period, String.Format(@"PAYSLIP-{0}.pdf", filename));
+
+                // myPDF.Attributes.Add("src", ResolveUrl("~/Downloads/" + String.Format(@"PAYSLIP-{0}.pdf", filename)));
+                if (File.Exists(filePath))
+                {
+                    System.Diagnostics.Debug.WriteLine("Payslip generated successfully.");
+                    myPDF.Attributes.Add("src", ResolveUrl("~/Downloads/" + String.Format("PAYSLIP-{0}.pdf", filename)));
+                }
+                else
+                {
+                    throw new FileNotFoundException("Payslip PDF was not found after generation.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Data.Clear();
+            }
+        }
+        private void ViewPayslip1()
         {
             try
             {
